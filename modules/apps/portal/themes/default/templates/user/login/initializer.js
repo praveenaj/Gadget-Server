c510a1db8df18ee66d811bd566509c28a6e18e34
nuwanbando <nuwan@bandara.co>
2012-10-09 12:06:58 +0530

    assemblyjs

--- modules/apps/portal/themes/default/templates/user/login/initializer.js
+++ modules/apps/portal/themes/default/templates/user/login/initializer.js
@@ -1,6 +1,6 @@
 Caramel.initializer("user/login", {
     preInitialize:function (data) {
         Caramel.addHeaderJS("user/login", "authjs", "templates/user/login/js/auth.js");
-        Caramel.addHeaderJS("user/login", "assembly", "templates/user/login/js/assembly.js")
+        Caramel.addHeaderJS("user/login", "assembly", "templates/user/login/js/assembly.js");
     }
 });

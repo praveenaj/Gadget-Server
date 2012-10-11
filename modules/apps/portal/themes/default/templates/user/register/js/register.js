60b716c6c8689f4e4516101e8ca935c5caab16b0
nuwanbando <nuwan@bandara.co>
2012-10-11 22:54:38 +0530

    pom changes

--- modules/apps/portal/themes/default/templates/user/register/js/register.js
+++ modules/apps/portal/themes/default/templates/user/register/js/register.js
@@ -1,15 +1,28 @@
 var User = User || ( function () {
     var register = function () {
-        $.post("/portal/apis/user/register/register.jag", {
-                action:"register",
-                username:$("#username").val(),
-                password:$("#password").val(),
-                fname:$("#fname").val(),
-                lname:$("#lname").val(),
-                email:$("#email").val()},
-            function (result) {
-                console.log(result)
-            }, "json");
+        var p1 = $("#password1").val();
+        var p2 = $("#password2").val();
+        if (p1 == p2) {
+            $.post("/portal/apis/user/register/register.jag", {
+                    action:"register",
+                    username:$("#username").val(),
+                    password:p1,
+                    fname:$("#fname").val(),
+                    lname:$("#lname").val(),
+                    email:$("#email").val()},
+                function (result) {
+                    console.log(result)
+                }, "json");
+        } else {
+            console.log("password miss match");
+        }
+        console.log({
+            action:"register",
+            username:$("#username").val(),
+            password:p1,
+            fname:$("#fname").val(),
+            lname:$("#lname").val(),
+            email:$("#email").val()});
     };
 
     var userExists = function () {

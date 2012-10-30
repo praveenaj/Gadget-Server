e25d79659d4172e1e7b6c81e2d2a523fa87a73a3
nuwanbando <nuwan@bandara.co>
2012-10-30 14:28:32 -0700

    removing log line in dashboard block

--- modules/apps/portal/blocks/dashboard/block.js
+++ modules/apps/portal/blocks/dashboard/block.js
@@ -7,9 +7,8 @@ Caramel.block("dashboard", {
     //The outputs of this block
     getOutputs:function (inputs) {
         var gadgetsMeta = Caramel.module("dashboard").getGadgetsForDashboard(Caramel.getUser(), inputs.dashName, inputs.gadgetArea);
-        log.info(gadgetsMeta.length);
+        //log.info(gadgetsMeta.length);
         var gadgets = [];
-
         for (var gadget in gadgetsMeta) {
             var gadgetHtml = Caramel.module("gadget").renderGadget(gadgetsMeta[gadget]);
             gadgets.push(gadgetHtml);
